b7da6430fdd1ec98b7c0901571862c4297d3b5dd
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-05-24 22:15:13 +0200

    dodanie migracji - klasy ktora uworzy baze danych

---
+++ Backend_CRUD/Migrations/202205241855581_CreateTables.cs
@@ -0,0 +1,61 @@
+﻿namespace Backend_CRUD.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTables : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Kategorias",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NazwaKategorii = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Kolors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NazwaKoloru = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Markas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NazwaMarki = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Ubranies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NazwaUbrania = c.String(),
+                        Cena = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Ilosc = c.Int(nullable: false),
+                        IdMarki = c.Int(nullable: false),
+                        IdKategorii = c.Int(nullable: false),
+                        IdKoloru = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Ubranies");
+            DropTable("dbo.Markas");
+            DropTable("dbo.Kolors");
+            DropTable("dbo.Kategorias");
+        }
+    }
+}
---
+++ Backend_CRUD/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Backend_CRUD.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Backend_CRUD.DB_Tables.MagazynDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Backend_CRUD.DB_Tables.MagazynDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
