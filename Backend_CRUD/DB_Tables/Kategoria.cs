fe2b04f140d911e1de7be8e64c2186c0e8be325c
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-05-24 22:08:44 +0200

    dodanie modeli tabel - Kolor, Marka, Ubranie, Kategoria

---
+++ Backend_CRUD/DB_Tables/Kategoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend_CRUD.DB_Tables
+{
+    public class Kategoria
+    {
+        public int Id { get; set; }
+        public string NazwaKategorii { get; set; }
+
+    }
+}
