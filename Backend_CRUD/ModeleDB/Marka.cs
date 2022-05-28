d2b701603653023d98b0bfc0d67af0834e8ec456
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-05-28 21:58:40 +0200

    zmiana nazwy folderu - dodanie ModeleDB

---
+++ Backend_CRUD/ModeleDB/Marka.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend_CRUD.DB_Tables
+{
+    public class Marka
+    {
+        public int Id { get; set; }
+        public string NazwaMarki { get; set; }
+    }
+}
