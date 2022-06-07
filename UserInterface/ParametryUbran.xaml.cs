b174a8b6c14a0201d3e02ceecc295e320e8011f6
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-06-07 22:34:17 +0200

    zabezpieczenie przed dodaniem pustego rekordu

--- UserInterface/ParametryUbran.xaml.cs
+++ UserInterface/ParametryUbran.xaml.cs
@@ -61,8 +61,13 @@ namespace UserInterface
 
         private void DodajKategorieBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Kategoria)KategoriaGrid.SelectedItem;
-            if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKategorii))
+            var item = KategoriaGrid.SelectedItem as Kategoria;
+
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKategorii))
             {
                 kategoriaRepozytorium.Add(item);
                 KategoriaGrid.ItemsSource = kategoriaRepozytorium.GetAll();
@@ -76,8 +81,13 @@ namespace UserInterface
 
         private void AktualizujKategorieBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Kategoria)KategoriaGrid.SelectedItem;
-            if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKategorii))
+            var item = KategoriaGrid.SelectedItem as Kategoria;
+
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKategorii))
             {
                 kategoriaRepozytorium.Update(item);
                 KategoriaGrid.ItemsSource = kategoriaRepozytorium.GetAll();
@@ -91,17 +101,29 @@ namespace UserInterface
 
         private void UsunKategorieBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Kategoria)KategoriaGrid.SelectedItem;
-            kategoriaRepozytorium.Remove(item.Id);
-            KategoriaGrid.ItemsSource = kategoriaRepozytorium.GetAll();
-            MessageBox.Show("Usuniete");
+            var item = KategoriaGrid.SelectedItem as Kategoria;
+
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else
+            {
+                kategoriaRepozytorium.Remove(item.Id);
+                KategoriaGrid.ItemsSource = kategoriaRepozytorium.GetAll();
+                MessageBox.Show("Usuniete");
+            }
         }
 
         private void DodajKolorBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Kolor)KolorGrid.SelectedItem;
+            var item = KolorGrid.SelectedItem as Kolor;
 
-            if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKoloru))
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKoloru))
             {
                 koloryRepozytorium.Add(item);
                 KolorGrid.ItemsSource = koloryRepozytorium.GetAll();
@@ -116,9 +138,13 @@ namespace UserInterface
 
         private void ZaktualizujKolorBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Kolor)KolorGrid.SelectedItem;
+            var item = KolorGrid.SelectedItem as Kolor;
 
-            if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKoloru))
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaKoloru))
             {
                 koloryRepozytorium.Update(item);
                 KolorGrid.ItemsSource = koloryRepozytorium.GetAll();
@@ -132,17 +158,29 @@ namespace UserInterface
 
         private void UsunKolorBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Kolor)KolorGrid.SelectedItem;
-            koloryRepozytorium.Remove(item.Id);
-            KolorGrid.ItemsSource = koloryRepozytorium.GetAll();
-            MessageBox.Show("Usuniete");
+            var item = KolorGrid.SelectedItem as Kolor;
+
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else
+            {
+                koloryRepozytorium.Remove(item.Id);
+                KolorGrid.ItemsSource = koloryRepozytorium.GetAll();
+                MessageBox.Show("Usuniete");
+            }
         }
 
         private void DodajMarkeBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Marka)MarkaGrid.SelectedItem;
+            var item = MarkaGrid.SelectedItem as Marka;
 
-            if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaMarki))
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaMarki))
             {
                 markaRepozytorium.Add(item);
                 MarkaGrid.ItemsSource = markaRepozytorium.GetAll();
@@ -156,9 +194,13 @@ namespace UserInterface
 
         private void ZaktualizujMarkeBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Marka)MarkaGrid.SelectedItem;
+            var item = MarkaGrid.SelectedItem as Marka;
 
-            if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaMarki))
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else if (WalidatorNapisu.CzyNapisNieJestPusty(item.NazwaMarki))
             {
                 markaRepozytorium.Update(item);
                 MarkaGrid.ItemsSource = markaRepozytorium.GetAll();
@@ -172,10 +214,18 @@ namespace UserInterface
 
         private void UsunMarkeBtn_Click(object sender, RoutedEventArgs e)
         {
-            var item = (Marka)MarkaGrid.SelectedItem;
-            markaRepozytorium.Remove(item.Id);
-            MarkaGrid.ItemsSource = markaRepozytorium.GetAll();
-            MessageBox.Show("Usuniete");
+            var item = MarkaGrid.SelectedItem as Marka;
+
+            if (item == null)
+            {
+                MessageBox.Show("Pusty rekord. Wprowadz dane");
+            }
+            else
+            {
+                markaRepozytorium.Remove(item.Id);
+                MarkaGrid.ItemsSource = markaRepozytorium.GetAll();
+                MessageBox.Show("Usuniete");
+            }
         }
 
         private void OdswiezWszystkieBtn_Click(object sender, RoutedEventArgs e)
