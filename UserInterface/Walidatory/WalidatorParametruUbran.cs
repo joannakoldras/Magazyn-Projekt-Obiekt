d71dee3387c6538165243bfe2c3d3999f118aa18
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-06-07 21:46:09 +0200

    dodanie walidatorow dla parametrow ubran

--- UserInterface/Walidatory/WalidatorParametruUbran.cs
+++ UserInterface/Walidatory/WalidatorParametruUbran.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Backend_CRUD.Repozytorium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,45 @@ namespace UserInterface.Walidatory
 {
     public static class WalidatorParametruUbran
     {
+        private static KolorRepozytorium koloryRepozytorium;
+        private static MarkaRepozytrium markaRepozytorium;
+        private static KategoriaRepozytrium kategoriaRepozytorium;
+
+        static WalidatorParametruUbran()
+        {
+            koloryRepozytorium = new KolorRepozytorium();
+            markaRepozytorium = new MarkaRepozytrium();
+            kategoriaRepozytorium = new KategoriaRepozytrium();
+        }
+
+        public static bool CzyIdKategoriiNieJestSpozaZakresu(int value)
+        {
+            var wynik = kategoriaRepozytorium.GetById(value);
+            if (wynik != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CzyIdKoloruNieJestSpozaZakresu(int value)
+        {
+            var wynik = koloryRepozytorium.GetById(value);
+            if (wynik != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CzyIdMarkiNieJestSpozaZakresu(int value)
+        {
+            var wynik = markaRepozytorium.GetById(value);
+            if (wynik != null)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
